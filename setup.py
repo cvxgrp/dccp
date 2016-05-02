414998e4d0de974d52976f029fc17cf8b6354563
Steven Diamond <stevend2@stanford.edu>
2016-05-02 20:55:12 +0200

    updates for bumpversion

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='dccp',
     version='0.1.0',
     author='Xinyue Shen, Steven Diamond, Stephen Boyd',
-     author_email='xinyues@stanford.edu, diamond@cs.stanford.edu, boyd@stanford.edu',
+    author_email='xinyues@stanford.edu, diamond@cs.stanford.edu, boyd@stanford.edu',
     packages=['dccp'],
     license='GPLv3',
     zip_safe=False,
