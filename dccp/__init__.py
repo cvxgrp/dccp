f505c569de55a630a96aec8a8488cad33718439b
Xinyue Shen <shenxinyue@cuhk.edu.cn>
2020-01-22 12:48:44 -0800

    update __init__.py

--- dccp/__init__.py
+++ dccp/__init__.py
@@ -3,5 +3,5 @@ from dccp.linearize import linearize
 from dccp.objective import convexify_obj
 from dccp.constraint import convexify_constr
 
-__author__ = 'Xinyue'
-__version___ = "1.0.0"
+__author__ = 'Xinyue Shen'
+__version__ = "1.0.0"
