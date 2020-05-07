dcc09ec9c772d4d3a9ce39b5eb1e6e0aa9b7a8b2
Steven Diamond <stevend2@stanford.edu>
2020-05-07 13:48:34 -0700

    Bump version: 1.0.0 → 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='dccp',
-    version='1.0.0',
+    version='1.0.1',
     author='Xinyue Shen, Steven Diamond, Stephen Boyd',
     author_email='xinyues@stanford.edu, diamond@cs.stanford.edu, boyd@stanford.edu',
     packages=['dccp'],
