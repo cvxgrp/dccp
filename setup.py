0286b98798b646ea1a8fc98de798bae3f6c3f61f
user799595 <101406722+user799595@users.noreply.github.com>
2023-06-06 14:02:29 +0100

    Removed 2to3 from setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,6 @@ setup(
     license="GPLv3",
     zip_safe=False,
     install_requires=["cvxpy >= 0.3.5"],
-    use_2to3=True,
     url="http://github.com/cvxgrp/dccp/",
     description="A CVXPY extension for difference of convex programs.",
 )
