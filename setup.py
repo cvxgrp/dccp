6c7490d710bc4cdde2e64170b4da074fe392ff3a
Steven Diamond <steven@gridmatic.com>
2023-11-27 14:36:21 -0800

    bump version

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name="dccp",
-    version="1.0.4",
+    version="1.0.5",
     author="Xinyue Shen, Steven Diamond, Stephen Boyd",
     author_email="xinyues@stanford.edu, diamond@cs.stanford.edu, boyd@stanford.edu",
     packages=["dccp"],
     license="GPLv3",
     zip_safe=False,
-    install_requires=["cvxpy >= 0.3.5"],
+    install_requires=["cvxpy >= 1.0"],
     url="http://github.com/cvxgrp/dccp/",
     description="A CVXPY extension for difference of convex programs.",
 )
