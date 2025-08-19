f59563144635dfe335d22a97bc36340e1903965b
Stefan de Lange <langestefan@msn.com>
2025-08-19 17:54:52 +0200

    Start rewriting problem.py

--- src/dccp/utils.py
+++ src/dccp/utils.py
@@ -1,8 +1,10 @@
 """DCCP utilities module."""
 
 import logging
+from dataclasses import dataclass
 
 import cvxpy as cp
+import numpy as np
 
 ORDER = "F"
 
@@ -11,12 +13,45 @@ LOGGER = logging.getLogger("dccp")
 LOGGER.setLevel(logging.DEBUG)
 
 
+@dataclass(kw_only=True)
+class DCCPSettings:
+    """Settings for the DCCP algorithm."""
+
+    max_iter: int = 100
+    tau_ini: float = 0.005
+    mu: float = 1.2
+    tau_max: float = 1e8
+    solver: str | None = None
+    k_ini: int = 1
+    k_ccp: int = 1
+    max_slack: float = 1e-3
+    ep: float = 1e-5
+    std: float = 10.0
+
+
 class NonDCCPError(Exception):
     """Exception raised when a problem is not DCCP compliant."""
 
     def __init__(self, message: str = "Problem is not DCCP compliant.") -> None:
         """Initialize NonDCCPError exception."""
         super().__init__(message)
+
+
+@dataclass
+class DCCPIter:
+    """Dataclass to hold DCCP iteration results."""
+
+    prob: cp.Problem
+    cost: float = np.inf
+    slack: float = np.inf
+
+    def __post_init__(self) -> None:
+        """Post-initialization."""
+
+    @property
+    def status(self) -> str:
+        """Return the status of the DCCP sub-problem."""
+        return self.prob.status
 
 
 def is_obj_dccp(objective: cp.Minimize | cp.Maximize) -> bool:
