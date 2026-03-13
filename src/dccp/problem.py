d75176a771369983df2d47e73bb2fd7d7b3cbd64
maximilianschaller <max@schaller.id>
2026-03-13 10:06:15 -0700

    silence linter

--- src/dccp/problem.py
+++ src/dccp/problem.py
@@ -303,7 +303,7 @@ class DCCP:
         # write the solution back to the original problem
         if converged:
             self.prob_in._status = cp.OPTIMAL  # noqa: SLF001
-            self.prob_in._value = self.iter.prob.value
+            self.prob_in._value = self.iter.prob.value  # noqa: SLF001
             for var in self.prob_in.variables():
                 var.value = self.iter.prob.var_dict[var.name()].value
             return self.iter.cost
